bb9afa1b29060c3f4c2b2161dfce2aa9293fd57d
Ashwin Dinesh <ashwin.dinesh@mirraw.com>
2017-09-28 20:11:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashwin/bottomtablayout/ItemTwoFragment.java
@@ -0,0 +1,28 @@
+package com.example.ashwin.bottomtablayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ashwin on 28/9/17.
+ */
+
+public class ItemTwoFragment extends Fragment {
+    public static ItemTwoFragment newInstance() {
+        ItemTwoFragment fragment = new ItemTwoFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_two, container, false);
+    }
+}
